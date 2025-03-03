03aac99cc627cc6daa95426ae652611c51fcb45a
fr0st <fr0st1s747@gmail.com>
2025-03-03 22:10:26 +0300

    init commit

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,11 @@ from rest_framework import routers
 from lms import views
 
 router = routers.DefaultRouter()
-router.register(r'courses', views.CourseViewSet)
+router.register(r'courses', views.CourseViewSet, basename='courses') #Укажите basename
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('lms.urls')), # Подключаем urls приложения lms
     path('api/users/', include('users.urls')),
+    path('api/', include('lms.urls')), # Добавьте это
 ]
--- lms/urls.py
+++ lms/urls.py
@@ -4,9 +4,9 @@ from . import views
 from .views import LessonListAPIView, LessonRetrieveAPIView, LessonCreateAPIView, LessonUpdateAPIView, LessonDestroyAPIView
 
 urlpatterns = [
-    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
-    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
-    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
-    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
-    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+    path('api/lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('api/lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+    path('api/lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('api/lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('api/lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ]
