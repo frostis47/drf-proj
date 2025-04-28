1081f0fa378a1596595a74cf2f248dd50c99dcc0
fr0st <fr0st1s747@gmail.com>
2025-04-28 18:45:54 +0300

    init commit

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('your_project_name')
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()  # Автоматически обнаруживаем и регистрируем Celery tasks во всех установленных Django apps
 
