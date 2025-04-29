1a89292cdff0004de344ef58896d62c238b1d9f0
fr0st <fr0st1s747@gmail.com>
2025-04-29 18:28:17 +0300

    init commit

--- config/settings.py
+++ config/settings.py
@@ -1,75 +1,76 @@
-
 import os
-import sys
-from datetime import timedelta
-from pathlib import Path
 from dotenv import load_dotenv
+from pathlib import Path
+from datetime import timedelta
+from celery.schedules import crontab
 
 load_dotenv()
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["*"]
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "rest_framework",
-    "rest_framework_simplejwt",
-    "users",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'corsheaders',
+    'drf_yasg',
+    'rest_framework_simplejwt',
+    'rest_framework',
     'lms',
-    "django_filters",
-    "drf_yasg",
+    'users',
+    'django_filters',
     'django_celery_beat',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = 'config.wsgi.application'
 
-REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
-}
+
+
 
 DATABASES = {
     'default': {
@@ -82,86 +83,100 @@ DATABASES = {
     }
 }
 
-if "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db.sqlite3",
-        }
-    }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
-LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Moscow"
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_URL = '/static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',  # Замените на адрес вашего фронтенд-сервера
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000', #  Замените на адрес вашего фронтенд-сервера
+    # и добавьте адрес бэкенд-сервера
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+DOMAIN = 'http://127.0.0.1:8000'
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
-CACHE_ENABLE = True
-if CACHE_ENABLE:
-    CACHES = {
-        "default": {
-            "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": "redis://localhost:6379/1",
-        }
-    }
-
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
-CELERY_TIMEZONE = TIME_ZONE
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_BEAT_SCHEDULE = {
-    "check_last_login": {
-        "task": "lms.tasks.check_last_login",
-        "schedule": timedelta(weeks=4),
-    }
+    'block-inactive-users': {
+        'task': 'users.tasks.block_inactive_users',
+        'schedule': crontab(hour=0, minute=1, day_of_month='1'),  # Запускать каждый месяц 1-го числа в 00:00
+    },
 }
+CELERY_BROKER_URL = f'redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB")}'
+CELERY_RESULT_BACKEND = f'redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB")}'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", True) == "False"
-
-SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- lms/tests.py
+++ lms/tests.py
@@ -1,124 +1,113 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from lms.models import Course, Lesson
+from users.models import User, Subscription
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import Group
 
-from users.models import User
-from .models import Course, Lesson, Subscription
-
-
-class LessonTestCase(APITestCase):
-
+class LMSAPITestCase(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(email="test@example.com")
-        self.course = Course.objects.create(name="moderator", description="Test")
-        self.lesson = Lesson.objects.create(
-            name="Django", course=self.course, owner=self.user
-        )
-        self.client.force_authenticate(user=self.user)
+        # Создаем пользователей
+        self.user = User.objects.create_user(email='test@example.com', password='testpassword')
+        self.moderator = User.objects.create_user(email='moderator@example.com', password='moderatorpassword')
+        moderator_group = Group.objects.create(name='moderators')
+        self.moderator.groups.add(moderator_group)
 
-    def test_create_lesson(self):
-        url = reverse("lms:lesson_create")
-        data = {
-            "name": "Test",
-            "description": "Test",
-            "course": self.course.pk,
-            "owner": self.user.pk,
-        }
-        response = self.client.post(url, data)
+        # Создаем токены
+        self.user_token = self.get_token(self.user)
+        self.moderator_token = self.get_token(self.moderator)
+
+        # Создаем курсы и уроки
+        self.course1 = Course.objects.create(title='Course 1', owner=self.user)
+        self.lesson1 = Lesson.objects.create(title='Lesson 1', course=self.course1, owner=self.user)
+
+    def get_token(self, user):
+        refresh = RefreshToken.for_user(user)
+        return {'access': str(refresh.access_token)}
+
+    def test_course_create(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        data = {'title': 'New Course', 'description': 'Description'}
+        response = self.client.post(reverse('course-list'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Course.objects.count(), 2)
+        self.assertEqual(Course.objects.last().title, 'New Course')
+        self.assertEqual(Course.objects.last().owner, self.user)
 
-    def test_create_lesson_with_youtube(self):
-        url = reverse("lms:lesson_create")
-        data = {
-            "name": "Test",
-            "description": "Test",
-            "course": self.course.pk,
-            "owner": self.user.pk,
-            "video_url": "https://www.youtube.com/",
-        }
-        response = self.client.post(url, data)
+    def test_course_list(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        response = self.client.get(reverse('course-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+
+    def test_course_update_by_owner(self):
+         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+         data = {'title': 'Updated Course'}
+         response = self.client.patch(reverse('course-detail', args=[self.course1.pk]), data)
+         self.assertEqual(response.status_code, status.HTTP_200_OK)
+         self.assertEqual(Course.objects.get(pk=self.course1.pk).title, 'Updated Course')
+
+    def test_course_update_by_not_owner(self):
+        user2 = User.objects.create_user(email='test2@example.com', password='testpassword')
+        token2 = self.get_token(user2)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token2["access"]}')
+        data = {'title': 'Updated Course by another user'}
+        response = self.client.patch(reverse('course-detail', args=[self.course1.pk]), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_lesson_create(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        data = {'title': 'New Lesson', 'course': self.course1.pk}
+        response = self.client.post(reverse('lesson-create'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_create_lesson_invalid_youtube(self):
-        url = reverse("lms:lesson_create")
-        data = {
-            "name": "Test",
-            "description": "Test",
-            "course": self.course.pk,
-            "owner": self.user.pk,
-            "video_url": "https://www.vk.com/",
-        }
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_lesson_retrieve(self):
-        url = reverse("lms:lesson_retrieve", args=(self.lesson.pk,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_lesson_update(self):
-        url = reverse("lms:lesson_update", args=(self.lesson.pk,))
-        data = {
-            "name": "Test1",
-            "description": "Test1",
-            "course": self.course.pk,
-            "owner": self.user.pk,
-        }
-        response = self.client.patch(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get("name"), "Test1")
-
-    def test_lesson_delete(self):
-        url = reverse("lms:lesson_delete", args=(self.lesson.pk,))
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Lesson.objects.count(), 2)
+        self.assertEqual(Lesson.objects.last().title, 'New Lesson')
+        self.assertEqual(Lesson.objects.last().course, self.course1)
+        self.assertEqual(Lesson.objects.last().owner, self.user)
 
     def test_lesson_list(self):
-        url = reverse("lms:lesson_list")
-        response = self.client.get(url)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        response = self.client.get(reverse('lesson-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
 
+    def test_lesson_update_by_owner(self):
+         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+         data = {'title': 'Updated Lesson'}
+         response = self.client.patch(reverse('lesson-detail', args=[self.lesson1.pk]), data)
+         self.assertEqual(response.status_code, status.HTTP_200_OK)
+         self.assertEqual(Lesson.objects.get(pk=self.lesson1.pk).title, 'Updated Lesson')
 
-class SubscriptionTestCase(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create(email="test@example.com")
-        self.course = Course.objects.create(name="moderator", description="Test")
-        self.client.force_authenticate(user=self.user)
+    def test_lesson_update_by_not_owner(self):
+        user2 = User.objects.create_user(email='test2@example.com', password='testpassword')
+        token2 = self.get_token(user2)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token2["access"]}')
+        data = {'title': 'Updated Lesson by another user'}
+        response = self.client.patch(reverse('lesson-detail', args=[self.lesson1.pk]), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_subscribe_to_course(self):
-        Subscription.objects.all().delete()
-        url = reverse("lms:subscription_create")
-        data = {"course_id": self.course.id}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["message"], "Вы подписались")
-        self.assertTrue(
-            Subscription.objects.filter(user=self.user, course=self.course).exists()
-        )
-
-        # Проверка отписки
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["message"], "Вы отписались")
+    def test_subscription_create(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        data = {'course': self.course1.pk} # changed course_id to course
+        response = self.client.post(reverse('subscriptions'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Subscription.objects.count(), 1)
+        self.assertEqual(Subscription.objects.first().user, self.user)
+        self.assertEqual(Subscription.objects.first().course, self.course1)
 
     def test_subscription_list(self):
-        url = reverse("lms:subscription_list")
-        response = self.client.get(url)
+        Subscription.objects.create(user=self.user, course=self.course1)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        response = self.client.get(reverse('subscriptions'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["course"], self.course.id)
+        self.assertEqual(response.data[0]['user'], self.user.pk)
+        self.assertEqual(response.data[0]['course'], self.course1.pk)
 
-    def test_subscribe_to_course_no_ex(self):
-        Subscription.objects.all().delete()
-        url = reverse("lms:subscription_create")
-        data = {"course_id": ""}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_subscribe_to_course_no_au(self):
-        Subscription.objects.all().delete()
-        self.client.force_authenticate(user=None)
-        url = reverse("lms:subscription_create")
-        data = {"course_id": self.course.id}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_subscription_delete(self):
+        subscription = Subscription.objects.create(user=self.user, course=self.course1)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.user_token["access"]}')
+        response = self.client.delete(reverse('subscription-delete', args=[subscription.pk]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Subscription.objects.count(), 0)
