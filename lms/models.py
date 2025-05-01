a2c3ec51c092fe16ddfd3232edce46acd6214859
fr0st <fr0st1s747@gmail.com>
2025-05-01 14:01:53 +0300

    init commit

--- lms/models.py
+++ lms/models.py
@@ -10,7 +10,7 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses',
-                              verbose_name='Владелец', null=True, blank=True)  # Изменено
+                              verbose_name='Владелец', null=True, blank=True)
     stripe_product_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='Stripe Product ID')
     stripe_price_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='Stripe Price ID')
     stripe_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Stripe Price',
@@ -45,13 +45,8 @@ class Lesson(models.Model):
 
 
 class CourseSubscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.email} - {self.course.title}"
-
-    class Meta:
-        verbose_name = "Подписка на курс"
-        verbose_name_plural = "Подписки на курсы"
-        unique_together = ('user', 'course')
