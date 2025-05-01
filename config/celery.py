8b142ede79a237dd6849d0de766dd7c936c244eb
fr0st <fr0st1s747@gmail.com>
2025-05-01 13:49:49 +0300

    init commit

--- config/celery.py
+++ config/celery.py
@@ -1,12 +1,20 @@
 import os
 from celery import Celery
 from dotenv import load_dotenv
+
+# Загружаем переменные окружения из файла .env
 load_dotenv()
 
+# Устанавливаем настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Создаем экземпляр Celery
 app = Celery('config')
+
+# Загружаем конфигурацию Celery из настроек Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Автоматически обнаруживаем задачи
 app.autodiscover_tasks()
 
 @app.task(bind=True)
@@ -15,3 +23,4 @@ def debug_task(self):
     Celery task для отладки. Выводит информацию о запросе.
     """
     print(f'Request: {self.request!r}')
+
