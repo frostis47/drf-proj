ae899676c82cf41dbfc0a9703a8934def839ecc9
fr0st <fr0st1s747@gmail.com>
2025-05-02 11:45:09 +0300

    init commit

--- lms/models.py
+++ lms/models.py
@@ -9,10 +9,12 @@ class Course(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses', verbose_name='Владелец', null=True, blank=True) # Изменено
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses',
+                              verbose_name='Владелец', null=True, blank=True) # Изменено
     stripe_product_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='Stripe Product ID')
     stripe_price_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='Stripe Price ID')
-    stripe_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Stripe Price', help_text='Цена в Stripe (для проверки изменений)')
+    stripe_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Stripe Price',
+                                       help_text='Цена в Stripe (для проверки изменений)')
 
     def __str__(self):
         return self.title
@@ -31,7 +33,8 @@ class Lesson(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True, verbose_name='Превью')
     video_link = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lessons', verbose_name='Владелец', null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lessons',
+                              verbose_name='Владелец', null=True, blank=True)
 
     def __str__(self):
         return self.title
