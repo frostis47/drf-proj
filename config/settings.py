899fa4c5308bf716764ed3078de83f053d87ae5a
fr0st <fr0st1s747@gmail.com>
2025-05-17 16:04:25 +0300

    init commit

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.User"
 
