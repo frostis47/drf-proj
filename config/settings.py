24a740d72417a66dc6807fd02f210b211fa7c95e
fr0st <fr0st1s747@gmail.com>
2025-05-18 19:51:04 +0300

    init commit

--- config/settings.py
+++ config/settings.py
@@ -10,12 +10,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-if not SECRET_KEY:
-    raise ValueError("The SECRET_KEY setting must not be empty.")
-
-DEBUG = os.getenv("DEBUG") == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -102,6 +99,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.User"
 
@@ -143,8 +141,12 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(hour=0, minute=1, day_of_month='1'),
     },
 }
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
